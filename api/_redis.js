56a0c8c41d6fab4754dc8f95a744be9ed0b0ead8
formywor <divedyacoub1@gmail.com>
2026-03-03 23:42:51 +0000

    ergger

--- api/_redis.js
+++ api/_redis.js
@@ -1,19 +1,28 @@
 const { Redis } = require("@upstash/redis");
 
 function getRedis() {
+
+  // Upstash KV integration variables
   const url =
+    process.env.KV_REST_API_URL ||
     process.env.STORAGE_REDIS_REST_URL ||
     process.env.UPSTASH_REDIS_REST_URL ||
     "";
 
   const token =
+    process.env.KV_REST_API_TOKEN ||
     process.env.STORAGE_REDIS_REST_TOKEN ||
     process.env.UPSTASH_REDIS_REST_TOKEN ||
     "";
 
-  if (!url || !token) throw new Error("missing_redis_env");
+  if (!url || !token) {
+    throw new Error("missing_redis_env");
+  }
 
-  return new Redis({ url, token });
+  return new Redis({
+    url: url,
+    token: token
+  });
 }
 
 module.exports = { getRedis };
