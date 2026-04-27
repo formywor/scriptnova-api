6013f252faaa70ef9ee1c45cbb0d639490af7562
formywor <divedyacoub1@gmail.com>
2026-04-27 18:39:11 +0000

    wcecewfwe

--- api/check.js
+++ api/check.js
@@ -152,7 +152,7 @@ async function resolveLicense(redis, lic) {
     return { ok: false, error: "invalid_key" };
   }
 
-  const allowedKeys = ["PRO-Z", "PRO-SNOVAPROKEY", "PRO-SENG1", "PRO-MH1", "PRO-MH2", "PRO-SHAYGAY", "PRO-KOFS"];
+  const allowedKeys = ["PRO-Z", "PRO-SNOVAPROKEY", "PRO-SENG1", "PRO-MH1", "PRO-MH2", "PRO-SHAYGAY", "PRO-KOFS", "PRO-SIGMATOILETHOWEY"];
   if (!allowedKeys.includes(lic.toUpperCase())) {
     return { ok: false, error: "invalid_key" };
   }
