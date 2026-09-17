14ef8fe10b46bf27ccb9737a0285bee674b348e0
formywor <divedyacoub1@gmail.com>
2026-09-17 23:15:10 +0000

    fhuygytc

--- test/galaxy.test.js
+++ test/galaxy.test.js
@@ -78,7 +78,7 @@ test("finishing is idempotent and removes the displayed Galaxy token", () => {
 
 test("Galaxy configuration is explicit about supported first-release capabilities", () => {
   const config = galaxy.configuration();
-  assert.equal(config.version, "1.0.2");
+  assert.equal(config.version, "1.0.3");
   assert.deepEqual(config.experiences, ["search", "partner", "browser"]);
   assert.equal(config.partner.name, "ScriptNovaa");
   assert.match(config.partner.label, /Sponsored/i);
